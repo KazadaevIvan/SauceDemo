19321f3ebd4f085faa6d69d99f8b00135c0c8654
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-09-27 16:41:14 +0300

    Added methods to click buttons

--- src/main/java/CartPage.java
+++ src/main/java/CartPage.java
@@ -1,9 +1,19 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class CartPage {
-    WebDriver driver;
+public class CartPage extends AbstractPage {
+    By continueShoppingButton = By.className("btn_secondary");
+    By checkoutButton = By.xpath("//a[@href='./checkout-step-one.html']");
 
     public CartPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
+    }
+
+    public void clickContinueShoppingButton() {
+        driver.findElement(continueShoppingButton).click();
+    }
+
+    public void clickCheckoutButton() {
+        driver.findElement(checkoutButton).click();
     }
 }
