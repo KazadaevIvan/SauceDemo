d8cf9238932d45f7814be2706064e9422100d9fd
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-09-29 22:53:29 +0300

    Added openPage abstract method

--- src/test/java/pages/AbstractPage.java
+++ src/test/java/pages/AbstractPage.java
@@ -1,22 +1,26 @@
+package pages;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 abstract public class AbstractPage {
+    public final static By OPEN_MENU_BUTTON = By.xpath("//button[contains(text(),'Open Menu')]");
+    public final static By SHOPPING_CART = By.cssSelector("[data-icon='shopping-cart']");
     WebDriver driver;
-    By openMenuButton = By.xpath("//button[text()='Open Menu']");
-    By shoppingCart = By.cssSelector("[data-icon='shopping-cart']");
 
     public AbstractPage(WebDriver driver) {
         this.driver = driver;
     }
 
+    abstract public void openPage();
+
     public MenuPage clickOpenMenuButton() {
-        driver.findElement(openMenuButton).click();
+        driver.findElement(OPEN_MENU_BUTTON).click();
         return new MenuPage(driver);
     }
 
     public CartPage openShoppingCart() {
-        driver.findElement(shoppingCart).click();
+        driver.findElement(SHOPPING_CART).click();
         return new CartPage(driver);
     }
 }
