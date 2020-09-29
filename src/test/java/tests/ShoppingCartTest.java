7ee4967e61ac6e15b12a61b0687fc48fc5aa6d46
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-09-29 23:07:55 +0300

    Updated ShoppingCartTest

--- src/test/java/tests/ShoppingCartTest.java
+++ src/test/java/tests/ShoppingCartTest.java
@@ -1,10 +1,11 @@
+package tests;
+
 import org.testng.annotations.Test;
 
 public class ShoppingCartTest extends BaseTest {
 
     @Test
     public void shoppingCartShouldBeOpened() {
-        CartPage cartPage = loginPage.login("standard_user", "secret_sauce")
-                .openShoppingCart();
+        cartPage.openPage();
     }
 }
