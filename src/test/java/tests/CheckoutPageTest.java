5af6a080bbc9703e8c7a4b2ae708c1745e10d775
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-09-30 12:41:00 +0300

    Added CheckoutPageTest

---
+++ src/test/java/tests/CheckoutPageTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CheckoutPageTest extends BaseTest {
+
+    @Test(dataProvider = "testDataForCheckout")
+    public void errorMessageShouldBeShownWhenCheckout(String firstName, String lastName, String postalCode, String errorMessage) {
+        checkoutPage.openPage();
+        checkoutPage.inputPersonalData(firstName, lastName, postalCode);
+        String actualResult = checkoutPage.getErrorMessageText();
+        assertEquals(actualResult, errorMessage, "Error error message should be '" + errorMessage + "'");
+    }
+
+    @DataProvider(name = "testDataForCheckout")
+    public Object[][] testDataForCheckout() {
+        return new Object[][]{
+                {"", "Ivanov", "22-04", "Error: First Name is required"},
+                {"Ivan", "", "22-04", "Error: Last Name is required"},
+                {"Ivan", "Ivanov", "", "Error: Postal Code is required"}
+        };
+    }
+}
