b391ff4026e56445ab2123ffbab0e3f44fbdb3e5
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-09-30 12:40:22 +0300

    Updated CHECKOUT_OVERVIEW_PAGE_URL

--- src/test/java/pages/CheckoutOverviewPage.java
+++ src/test/java/pages/CheckoutOverviewPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CheckoutOverviewPage extends AbstractPage {
-    public static final String CHECKOUT_OVERVIEW_PAGE_URL = "https://www.saucedemo.com/checkout-step-two.html";
+    public final static String CHECKOUT_OVERVIEW_PAGE_URL = "https://www.saucedemo.com/checkout-step-two.html";
     public final static By CANCEL_BUTTON = By.xpath("//button[@contains(text(),'CANCEL']");
     public final static By FINISH_BUTTON = By.xpath("//button[@contains(text(),'FINISH']");
 
