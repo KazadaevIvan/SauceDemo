85c3d116ea576fda424a60b015df533e4fc0b785
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-09-30 12:39:19 +0300

    Added DataProvider to errorMessageShouldBeShownWhenLogin test. Added userShouldBeLogined test

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -1,17 +1,33 @@
 package tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class LoginPageTest extends BaseTest {
 
-    @Test
-    public void errorMessageShouldBeShown() {
+    @Test(dataProvider = "testDataForLogin")
+    public void errorMessageShouldBeShownWhenLogin(String username, String password, String errorMessage) {
         loginPage.openPage();
-        loginPage.login(USERNAME, "");
+        loginPage.login(username, password);
         String actualResult = loginPage.getErrorMessageText();
-        String expectedResult = "Epic sadface: Password is required";
-        assertEquals(actualResult, expectedResult, "Error message should be '" + expectedResult + "'");
+        assertEquals(actualResult, errorMessage, "Error error message should be '" + errorMessage + "'");
+    }
+
+    @DataProvider(name = "testDataForLogin")
+    public Object[][] testDataForLogin() {
+        return new Object[][]{
+                {USERNAME, "", "Epic sadface: Password is required"},
+                {"", PASSWORD, "Epic sadface: Username is required"},
+                {"admin", "12345678", "Epic sadface: Username and password do not match any user in this service"}
+        };
+    }
+
+    @Test
+    public void userShouldBeLogined() {
+        loginPage.openPage();
+        loginPage.login(USERNAME, PASSWORD);
+        assertEquals(driver.getCurrentUrl(), productsPage.getURL(), "URL should be '" + productsPage.getURL() + "'");
     }
 }
