d95041d389247f6b76aa0b8491d68667aebf3d0d
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-10-07 12:07:53 +0300

    Added productShouldBeAddedToCart method

--- src/test/java/tests/ItemInfoPageTest.java
+++ src/test/java/tests/ItemInfoPageTest.java
@@ -5,12 +5,25 @@ import org.testng.annotations.Test;
 public class ItemInfoPageTest extends BaseTest {
 
     @Test
+    public void productShouldBeAddedToCart() {
+        String itemName = "Sauce Labs Backpack";
+        productsPage
+                .openPage()
+                .isPageOpened()
+                .openItemInfo(itemName)
+                .clickAddToCartButton()
+                .openShoppingCart();
+        cartPage
+                .isPageOpened()
+                .productDetailsShouldBeLike("Sauce Labs Backpack", "1", "29.99");
+    }
+
+    @Test
     public void productShouldBeRemovedFromCart() {
         String itemName = "Sauce Labs Backpack";
         productsPage
                 .openPage()
                 .isPageOpened()
-                .addItemToCart("Sauce Labs Bike Light")
                 .openItemInfo(itemName)
                 .clickAddToCartButton()
                 .openShoppingCart();
