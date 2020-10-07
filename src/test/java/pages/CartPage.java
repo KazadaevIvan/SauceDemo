7d16ea5a6ee38081e5e03b8efb45d40cf37ee774
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-10-07 11:45:37 +0300

    Added isCartEmpty and removeItemFromCart methods

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -12,10 +12,12 @@ public class CartPage extends AbstractPage {
     public final static String CART_PAGE_URL = "cart.html";
     public final static By CONTINUE_SHOPPING_BUTTON = By.xpath("//*[contains(text(),'Continue')]");
     public final static By CHECKOUT_BUTTON = By.cssSelector(".checkout_button");
+    public final static By CART_ITEM = By.cssSelector(".cart_item");
     String priceLocator = "//*[contains(text(),'%s')]/ancestor::*[@class='cart_item']" +
             "//div[@class='inventory_item_price']";
     String quantityLocator = "//*[contains(text(),'%s')]/ancestor::*[@class='cart_item']" +
             "//div[@class='cart_quantity']";
+    String removeItemFromCartLocator = "//*[contains(text(),'%s')]/ancestor::div[@class='cart_item']//button";
 
     public CartPage(WebDriver driver) {
         super(driver);
@@ -36,7 +38,7 @@ public class CartPage extends AbstractPage {
         return this;
     }
 
-    public ProductsPage clickContinueShoppingButton() {
+    public ProductsPage continueShopping() {
         driver.findElement(CONTINUE_SHOPPING_BUTTON).click();
         return new ProductsPage(driver);
     }
@@ -58,5 +60,21 @@ public class CartPage extends AbstractPage {
 
     public String getProductQuantity(String productName) {
         return driver.findElement(By.xpath(String.format(quantityLocator, productName))).getText();
+    }
+
+
+
+    public CartPage isCartEmpty() {
+        try {
+            wait.until(ExpectedConditions.numberOfElementsToBe(CART_ITEM, 0));
+        } catch (TimeoutException e) {
+            Assert.fail("Корзина не пуста. Количество элементов в корзине: " + driver.findElements(CART_ITEM).size());
+        }
+        return this;
+    }
+
+    public CartPage removeItemFromCart(String productName) {
+        driver.findElement(By.xpath(String.format(removeItemFromCartLocator, productName))).click();
+        return this;
     }
 }
