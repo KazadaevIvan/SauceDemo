c715eeed1e19ebd5055962f6af9b77db210e2de6
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-10-07 11:49:34 +0300

    Added productShouldBeRemovedFromCart method

--- src/test/java/tests/ItemInfoPageTest.java
+++ src/test/java/tests/ItemInfoPageTest.java
@@ -1,5 +1,30 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class ItemInfoPageTest extends BaseTest {
 
+    @Test
+    public void productShouldBeRemovedFromCart() {
+        String itemName = "Sauce Labs Backpack";
+        productsPage
+                .openPage()
+                .isPageOpened()
+                .addItemToCart("Sauce Labs Bike Light")
+                .openItemInfo(itemName)
+                .clickAddToCartButton()
+                .openShoppingCart();
+        cartPage
+                .isPageOpened()
+                .productDetailsShouldBeLike("Sauce Labs Backpack", "1", "29.99")
+                .continueShopping();
+        productsPage
+                .isPageOpened()
+                .openItemInfo(itemName)
+                .clickRemoveFromCartButton()
+                .openShoppingCart();
+        cartPage
+                .isPageOpened()
+                .isCartEmpty();
+    }
 }
