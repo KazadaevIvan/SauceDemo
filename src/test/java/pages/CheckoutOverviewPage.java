84d1f3cc05bf14deeaea37a888472db507c0ed51
i.kazadaev <i.kazadaev@wannaplay.gg>
2020-10-07 13:24:10 +0300

    Added productDetailsShouldBeLike, getProductPrice, getProductPrice, getProductPrice, getItemsTotalPrice, itemsTotalPriceShouldBeLike methods

--- src/test/java/pages/CheckoutOverviewPage.java
+++ src/test/java/pages/CheckoutOverviewPage.java
@@ -3,13 +3,25 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 public class CheckoutOverviewPage extends AbstractPage {
     public final static String CHECKOUT_OVERVIEW_PAGE_URL = "checkout-step-two.html";
     public final static By CANCEL_BUTTON = By.cssSelector(".btn_secondary");
     public final static By FINISH_BUTTON = By.cssSelector(".btn_action");
+    public final static By CART_ITEM_PRICE = By.cssSelector(".inventory_item_price");
+    public final static By ITEM_TOTAL_PRICE = By.className("summary_subtotal_label");
+    String priceLocator = "//*[contains(text(),'%s')]/ancestor::*[@class='cart_item']" +
+            "//div[@class='inventory_item_price']";
+    String quantityLocator = "//*[contains(text(),'%s')]/ancestor::*[@class='cart_item']" +
+            "//div[@class='summary_quantity']";
 
     public CheckoutOverviewPage(WebDriver driver) {
         super(driver);
@@ -38,5 +50,37 @@ public class CheckoutOverviewPage extends AbstractPage {
     public FinishPage finishButtonClick() {
         driver.findElement(FINISH_BUTTON).click();
         return new FinishPage(driver);
+    }
+
+    public CheckoutOverviewPage productDetailsShouldBeLike(String productName, String quantity, String price) {
+        assertEquals(getProductPrice(productName), price, "Price is not correct");
+        assertEquals(getProductQuantity(productName), quantity, "Quantity is not correct");
+        return this;
+    }
+
+    public String getProductPrice(String productName) {
+        return driver.findElement(By.xpath(String.format(priceLocator, productName))).getText().substring(1);
+    }
+
+    public String getProductQuantity(String productName) {
+        return driver.findElement(By.xpath(String.format(quantityLocator, productName))).getText();
+    }
+
+    public Double getSumOfAllItemsPrices() {
+        List<WebElement> items = driver.findElements(CART_ITEM_PRICE);
+        double sum = 0;
+        for (WebElement element : items) {
+            sum += Double.parseDouble(element.getText().substring(1));
+        }
+        return sum;
+    }
+
+    public Double getItemsTotalPrice() {
+        return Double.parseDouble(driver.findElement(ITEM_TOTAL_PRICE).getText().substring(13));
+    }
+
+    public CheckoutOverviewPage itemsTotalPriceShouldBeLike() {
+        assertEquals(getSumOfAllItemsPrices(), getItemsTotalPrice(), "Total price is not correct");
+        return this;
     }
 }
