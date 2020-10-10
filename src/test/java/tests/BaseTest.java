0e8d0a864417a43139fd0cbf1fa2162b9cf950f3
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-10 13:13:08 +0300

    Added Listener

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import pages.*;
 
 import java.util.concurrent.TimeUnit;
 
+@Listeners(TestListener.class)
 public class BaseTest {
 
     public static final String USERNAME = "standard_user";
