47d743f9c1985dacb77bac074b53869faeb3def5
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-27 17:19:57 +0300

    Added takeScreenshot method

--- src/test/java/tests/TestListener.java
+++ src/test/java/tests/TestListener.java
@@ -1,5 +1,10 @@
 package tests;
 
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.NoSuchSessionException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -24,6 +29,7 @@ public class TestListener implements ITestListener {
     public void onTestFailure(ITestResult iTestResult) {
         System.out.println(String.format("======================================== FAILED TEST %s Duration: %ss ========================================", iTestResult.getName(),
                 getExecutionTime(iTestResult)));
+        takeScreenshot(iTestResult);
     }
 
     @Override
@@ -44,6 +50,21 @@ public class TestListener implements ITestListener {
     @Override
     public void onFinish(ITestContext iTestContext) {
 
+    }
+
+    @Attachment(value = "Last screen state", type = "image/png")
+    private byte[] takeScreenshot(ITestResult iTestResult) {
+        ITestContext context = iTestResult.getTestContext();
+        try {
+            WebDriver driver = (WebDriver) context.getAttribute("driver");
+            if (driver != null) {
+                return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+            } else {
+                return new byte[] {};
+            }
+        } catch (NoSuchSessionException | IllegalStateException ex) {
+            return null;
+        }
     }
 
     private long getExecutionTime(ITestResult iTestResult) {
