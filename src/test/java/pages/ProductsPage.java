4afeb433a8feb32bbe70235441f8352543b17ab3
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-27 17:22:59 +0300

    Deleted sorting methods

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -1,15 +1,14 @@
 package pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ProductsPage extends AbstractPage {
@@ -25,11 +24,13 @@ public class ProductsPage extends AbstractPage {
         super(driver);
     }
 
+    @Step("Opening Products page")
     public ProductsPage openPage() {
         driver.get(URL + PRODUCTS_PAGE_URL);
         return this;
     }
 
+    @Step("Verify Products page is opened")
     @Override
     public ProductsPage isPageOpened() {
         try {
@@ -72,19 +73,16 @@ public class ProductsPage extends AbstractPage {
         return prices;
     }
 
+    @Step("Finding product and click ADD button")
     public ProductsPage addItemToCart(String itemName) {
         driver.findElement(By.xpath(String.format(addRemoveToCartLocator, itemName))).click();
         return this;
     }
 
+    @Step("Finding product '{itemName}' and click REMOVE button")
     public ProductsPage removeItemFromCart(String itemName) {
         driver.findElement(By.xpath(String.format(addRemoveToCartLocator, itemName))).click();
         return this;
-    }
-
-    public void chooseSortingMethod(String value) {
-        Select sortingMethodSelector = new Select(driver.findElement(SORTING_METHOD));
-        sortingMethodSelector.selectByVisibleText(value);
     }
 
     public void printAllItemsNamesWithPrices() {
@@ -93,27 +91,5 @@ public class ProductsPage extends AbstractPage {
         for (int i = 0; i < names.size(); i++) {
             System.out.println(names.get(i).getText() + " - " + prices.get(i).getText());
         }
-    }
-
-    public String sortItemsNamesFromAToZ() {
-        Collections.sort(getAllItemsNames());
-        return getAllItemsNames().toString();
-    }
-
-    public String sortItemsNamesFromZToA() {
-        Collections.sort(getAllItemsNames());
-        Collections.reverse(getAllItemsNames());
-        return getAllItemsNames().toString();
-    }
-
-    public String sortItemsPricesFromLowToHigh() {
-        Collections.sort(getAllItemsPrices());
-        return getAllItemsPrices().toString();
-    }
-
-    public String sortItemsPricesFromHighToLow() {
-        Collections.sort(getAllItemsPrices());
-        Collections.reverse(getAllItemsPrices());
-        return getAllItemsPrices().toString();
     }
 }
