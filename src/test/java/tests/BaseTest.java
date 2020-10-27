5aaf52d5390645f13402e46d0e8e21834d4ed606
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-27 17:25:09 +0300

    Added ProductPageSteps and CartPageSteps instances

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,6 +9,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 import pages.*;
+import steps.CartPageSteps;
+import steps.ProductPageSteps;
 
 import java.util.concurrent.TimeUnit;
 
@@ -23,27 +26,33 @@ public class BaseTest {
     CheckoutPage checkoutPage;
     CheckoutOverviewPage checkoutOverviewPage;
     FinishPage finishPage;
+    ProductPageSteps productPageSteps;
+    CartPageSteps cartPageSteps;
 
     @BeforeSuite
     public void beforeSuite() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
     }
 
+    @Step("Open browser")
     @BeforeMethod
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
         checkoutPage = new CheckoutPage(driver);
         checkoutOverviewPage = new CheckoutOverviewPage(driver);
         finishPage = new FinishPage(driver);
+        productPageSteps = new ProductPageSteps(driver);
+        cartPageSteps = new CartPageSteps(driver);
     }
 
+    @Step("Close browser")
     @AfterMethod
     public void tearDown() {
         driver.quit();
