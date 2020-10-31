d2497915113c48740ccf37fa992e17148858290e
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-31 13:08:49 +0300

    Added openPage and isPageOpened methods

--- src/test/java/pages/MenuPage.java
+++ src/test/java/pages/MenuPage.java
@@ -2,18 +2,36 @@ package pages;
 
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 
-public class MenuPage {
+public class MenuPage extends AbstractPage {
     public final static By ALL_ITEMS_OPTION = By.id("inventory_sidebar_link");
     public final static By ABOUT_OPTION = By.id("about_sidebar_link");
     public final static By LOGOUT_OPTION = By.id("logout_sidebar_link");
     public final static By RESET_APP_STATE_OPTION = By.id("reset_sidebar_link");
     public final static By CLOSE_BUTTON = By.xpath("//button[text()='Close Menu']");
-    WebDriver driver;
 
     public MenuPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
+    }
+
+    @Override
+    public MenuPage openPage() {
+        System.out.println("Don't do this");
+        return this;
+    }
+
+    @Override
+    public MenuPage isPageOpened() {
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(LOGOUT_OPTION));
+        } catch (TimeoutException e) {
+            Assert.fail("The page has not been loaded. Button has not been found by locator " + LOGOUT_OPTION);
+        }
+        return this;
     }
 
     @Step("Click All Items option")
@@ -23,8 +41,9 @@ public class MenuPage {
     }
 
     @Step("Click About option")
-    public void clickAboutOption() {
+    public AboutPage clickAboutOption() {
         driver.findElement(ABOUT_OPTION).click();
+        return new AboutPage(driver);
     }
 
     @Step("Click Logout option")
